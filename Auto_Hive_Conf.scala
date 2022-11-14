89653d33b466d0b56245f3f5bdda162c3b7dd579
krisd <krisdong88@gmail.com>
2022-11-14 17:34:52 -0500

    Updated again

--- Auto_Hive_Conf.scala
+++ Auto_Hive_Conf.scala
@@ -19,6 +19,28 @@ class Auto_Hive_Conf {
         "spark-class org.apache.spark.deploy.worker.Worker spark://" + ip +":7077")
       printWriter.close() // Closing printwriter
     }
+
+    def write_submit_nodes_bat(): Unit = {
+      val ip = tracker.track_ip()
+      val fileObject = new File("submit_job.bat")
+      val classs = "org.apache.spark.examples.SparkPi"
+      val master = "spark://192.168.1.14:7077"
+      val deploy_mode = "cluster"
+      val executor_memory = "20G"
+      val total_executor_cores = "100"
+      val printWriter = new PrintWriter(fileObject)
+      printWriter.write(
+        "./bin/spark-submit \n" +
+        "--class " + classs + " \n"+
+        "--master " + master + " \n"+
+        "--deploy-mode " + deploy_mode + " \n"+
+        "--supervise \n" +
+        "--executor-memory " + executor_memory + " \n" +
+        "--total-executor-cores " + total_executor_cores + " \n" +
+        "/path/to/examples.jar \n" +
+        "1000")
+      printWriter.close()
+    }
 }
 
 object Test {
@@ -26,6 +48,7 @@ object Test {
     var a= new Auto_Hive_Conf()
     a.write_master_nodes_bat()
     a.write_worker_nodes_bat()
+    a.write_submit_nodes_bat()
    }
 
 }
