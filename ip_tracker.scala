a7525ae3d73adc54703d8fd9ddcf5d8e337d4156
krisd <krisdong88@gmail.com>
2022-11-13 19:03:15 -0500

    Created ip_tracker.scala which you could track your current IP address with this scala class updated

--- ip_tracker.scala
+++ ip_tracker.scala
@@ -1,8 +1,20 @@
 import java.net._
 object ip_tracker {
   def main(args: Array[String]) = {
-    val localhost: InetAddress = InetAddress.getLocalHost
-    val localIpAddress: String = localhost.getHostAddress
-    println(s"localIpAddress = $localIpAddress")
+    val e = NetworkInterface.getNetworkInterfaces
+    var res: List[String] = List()
+    while ( {
+      e.hasMoreElements
+    }) {
+      val n = e.nextElement
+      val ee = n.getInetAddresses
+      while ( {
+        ee.hasMoreElements
+      }) {
+        val i = ee.nextElement
+        res = res :+ i.getHostAddress
+      }
+    }
+    println(res(res.length - 5))
   }
 }
