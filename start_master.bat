c91c18d20802535d38f64e2ff185127da345e080
krisd <krisdong88@gmail.com>
2022-11-24 21:21:54 -0500

    create a GUI file

--- start_master.bat
+++ start_master.bat
@@ -1,3 +1,1 @@
-for /F "tokens=2 delims=:" %%i in ('"ipconfig | findstr IPv4 | findstr 192.168.1."') do SET LOCAL_IP=%%i
-spark-class org.apache.spark.deploy.master.Master -h %LOCAL_IP%
-echo
+spark-class org.apache.spark.deploy.master.Master -h 192.168.1.153
