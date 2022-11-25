eb72c173b7646f5e8387d50ba9d31f4702efacc6
NengZhou <Vizards8@outlook.com>
2022-11-24 21:34:25 -0500

    modify WordCount.scala

--- task/WordCount.scala
+++ task/WordCount.scala
@@ -1,17 +1,10 @@
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.SparkContext
-import org.apache.spark.ml.feature.VectorAssembler
-
-/**
- * @author JavaEdge
- * @date 2019-04-09
- *
- */
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org.apache").setLevel(Level.WARN);
+    Logger.getLogger("org.apache").setLevel(Level.WARN)
 
     //    val sc = new SparkContext("spark://192.168.1.14:7077", "WordCount")
     val sc = new SparkContext("local[*]", "WordCount")
@@ -19,8 +12,13 @@ object WordCount {
     val file = sc.textFile("D:/test.txt")
 
     // 先分割成单词数组，然后合并，再与1形成KV映射
-    val result = file.flatMap(_.split(" ")).map((_, 1)).reduceByKey((a, b) => a + b).sortBy(_._2)
-    result.foreach(println(_))
+    val result = file
+      .flatMap(value => value.split(" "))
+      .map(value => (value, 1))
+      .reduceByKey((a, b) => a + b)
+      .sortBy(value => -value._2)
+
+    result.foreach(value => println(value))
   }
 
 }
