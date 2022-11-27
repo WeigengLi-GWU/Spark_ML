0a1600cdc0323cda5d9843bfdf87ec0cefaecfdc
WeigengLi <liweigeng2014@hotmail.com>
2022-11-27 18:34:19 -0500

    functions for changing data into one hot code

---
+++ src/main/scala/com/wic/ml/Preprocess.scala
@@ -0,0 +1,27 @@
+package com.wic.ml
+
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Preprocess {
+  def to_onehot(dataframe: DataFrame, col_name: String): DataFrame = {
+    val strIndexer = new StringIndexer
+    strIndexer.setInputCol(col_name)
+    strIndexer.setOutputCol(col_name + "Index")
+    var newdata = strIndexer.fit(dataframe).transform(dataframe)
+    newdata = newdata.drop(col_name)
+    val ohEncoder = new OneHotEncoder
+    ohEncoder.setInputCols(Array[String](col_name + "Index"))
+    ohEncoder.setOutputCols(Array[String](col_name))
+    newdata = ohEncoder.fit(newdata).transform(newdata)
+    newdata = newdata.drop(col_name + "Index")
+    return newdata
+  }
+  def to_onehot(data: DataFrame, col_name: Array[String]): DataFrame = {
+    var newdata = to_onehot(data,(col_name(0)).toString())
+    for (i <- 1 to col_name.length-1){
+      newdata = to_onehot(newdata,col_name(i))
+    }
+    return newdata
+  }
+}
